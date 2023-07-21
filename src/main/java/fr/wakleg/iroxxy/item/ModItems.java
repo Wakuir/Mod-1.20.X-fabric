ca7efaab7aa0da0244aa61109880b80e470f09df
Wakuir <113540865+Wakuir@users.noreply.github.com>
2023-07-21 11:20:47 +0200

    Metal Detector added

--- src/main/java/fr/wakleg/iroxxy/item/ModItems.java
+++ src/main/java/fr/wakleg/iroxxy/item/ModItems.java
@@ -1,6 +1,7 @@
 package fr.wakleg.iroxxy.item;
 
 import fr.wakleg.iroxxy.Main;
+import fr.wakleg.iroxxy.item.custom.MetalDetectorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -13,6 +14,8 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries){
         entries.add(RUBY);
